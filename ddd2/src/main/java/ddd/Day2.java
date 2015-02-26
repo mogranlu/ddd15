69a00e78d55f676cb0de462711e61043daeffb4b
Morten Granlund <morten.granlund@gmail.com>
2015-02-26 07:34:51 +0100

    Renamed dummy test case for day 2, and setup for hamcrest tests

---
+++ ddd2/src/main/java/ddd/Day2.java
@@ -0,0 +1,17 @@
+package ddd;
+
+public class Day2 {
+	
+	
+	public Day2() {
+		
+	}
+
+	public void somethingSomething() throws UnsupportedOperationException {
+		throw new UnsupportedOperationException("not yet implemented");
+		
+	}
+	
+	
+	
+}
