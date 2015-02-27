9775b4e413d51a81b24f6577b188f9bb28003a25
Sanjin Cevro <sanjinc@gmail.com>
2015-02-27 10:58:11 +0100

    Added more code...

--- ddd2/src/main/java/ddd/EnrollApplicationService.java
+++ ddd2/src/main/java/ddd/EnrollApplicationService.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 public class EnrollApplicationService {
 	private IClassRepository classRepository;
 	private IStudentRepository studentRepository;
-	private ScheduleService scheduleService;
 	private ICanCheckPaymentStatus paymentStatusChecker;
 
 	public EnrollApplicationService(IStudentRepository studRepo, IClassRepository classRepo, ICanCheckPaymentStatus paymentChecker) {
@@ -16,8 +15,10 @@ public class EnrollApplicationService {
 	
 	public void enroll(UUID studentId, UUID classId) {
 		Student s = studentRepository.getStudentById(studentId);
-		
-		
+		Class class1 = classRepository.getClassById(classId);
+
+        ScheduleService scheduleService1 = new ScheduleService();
+        scheduleService1.enrollStudentInClass(s, class1);
 	}
 	
 	
