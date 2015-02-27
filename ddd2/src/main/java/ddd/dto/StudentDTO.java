90e83ea09309cd1d451cf7850ac77d1f997f6fc5
Sanjin Cevro <sanjinc@gmail.com>
2015-02-27 13:03:47 +0100

    Added getters.

--- ddd2/src/main/java/ddd/dto/StudentDTO.java
+++ ddd2/src/main/java/ddd/dto/StudentDTO.java
@@ -5,7 +5,32 @@ import java.util.UUID;
 
 public class StudentDTO {
 
-    public StudentDTO(UUID id, String name, String birthday, String gender){
+    private final UUID id;
+    private final String name;
+    private final String birthday;
+    private final String gender;
 
+    public StudentDTO(UUID id, String name, String birthday, String gender){
+        this.id = id;
+        this.name = name;
+        this.birthday = birthday;
+        this.gender = gender;
     }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
 }
