5564ce603dd4e8497b701c83b89bdd0d7f13b10f
Morten Granlund <morten.granlund@gmail.com>
2015-02-27 11:09:47 +0100

    Hooked the TeacherApplicationService to the teacher (domain) services.

---
+++ ddd2/src/main/java/ddd/AssignApplicationService.java
@@ -0,0 +1,26 @@
+package ddd;
+
+import java.util.UUID;
+
+public class AssignApplicationService {
+
+
+	private IClassRepository classRepository;
+	private ITeacherRepository teacherRepository;
+
+	public AssignApplicationService(ITeacherRepository teacherRepo, IClassRepository classRepo) {
+		classRepository = classRepo;
+		teacherRepository = teacherRepo;
+	}
+	
+	public void enroll(UUID teacherId, UUID classId) {
+		Teacher teacher = teacherRepository.getTeacherById(teacherId);
+		Class class1 = classRepository.getClassById(classId);
+
+		AssignService assignService = new AssignService(); 
+        assignService.assignTeacherToAClass(teacherId, classId);
+	}
+	
+
+	
+}
